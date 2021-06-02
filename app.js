b6fca80eae912ecc0cec338d9b9fa8fa9e7db26f
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2021-06-02 00:12:31 -0400

    finished the grid generation and color randomizer effect. need to add title, reset button and lock to 50 by 50 for performance issues

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const container = document.getElementById('container')
-
-//function to add divs to container
+const grid = document.getElementsByClassName('grid');
+//function to add divs to container at specified size
 
 function makeGrid(length,width) {
     let i;
     //makes the divs based on length and width
  for(i=0; i <(length*width); i++) {
      let div = document.createElement('div');
-     div.className = "grid";
+     div.className = 'grid';
      container.appendChild(div);
  }
  //adjusts the css style of container to fit divs in 800px by 800px
@@ -15,18 +15,27 @@ function makeGrid(length,width) {
  container.style.gridTemplateRows = `repeat(${length},${800/length}px)`;
  
 }
+ makeGrid(50,50);
  
 
+//adding hover eventListeners to grid divs
 
-makeGrid(16,16);
+for(let i = 0; i <grid.length; i++){
+    grid[i].addEventListener('mouseover', function() {
+        let color = randomRGB;
+        grid[i].style.backgroundColor = `${randomRGB()}`;
+    })
+}
+
+
 
 //Helper Functions//
 
-///returns 3 RGB values (0-255) in an array
+///returns value between 0-255
 function randomRGB(){
     let r = Math.round(Math.random() * 255);
     let g = Math.round(Math.random() * 255);
     let b = Math.round(Math.random() * 255);
-    return [r,g,b]; 
+    return `rgb(${r},${g},${b})`;
 }
 
